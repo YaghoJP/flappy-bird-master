bdcdba180f7de0bcf1ab917c15d19b336aee5bd0
Yagho Petini <yaghojp@gmail.com>
2025-03-29 22:49:23 +0000

    Implementando o Ground

---
+++ Ground/ground.gd
@@ -0,0 +1,25 @@
+extends Node2D
+class_name Ground
+
+@export_category("Objects")
+@export var ground_1: Area2D
+@export var ground_2: Area2D
+@export var textureGround_1: Sprite2D
+
+@export_category("Variables")
+@export var speed: float = -150.0
+
+var width: int
+
+func _ready() -> void:
+	width = textureGround_1.texture.get_width()
+	
+func _process(delta: float) -> void:
+	ground_1.global_position.x += speed * delta
+	ground_2.global_position.x += speed * delta
+	
+	if ground_1.global_position.x < -int(width/2):
+		ground_1.global_position.x = ground_2.global_position.x + width
+
+	if ground_2.global_position.x < -int(width/2):
+		ground_2.global_position.x = ground_1.global_position.x + width
